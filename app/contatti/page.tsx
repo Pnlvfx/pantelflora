5c60dc8ac028a6f7572691c9fd221ce644e67b79
simone <simonegauli@gmail.com>
2025-02-10 18:59:42 +0100

    fix contact page

--- app/contatti/page.tsx
+++ app/contatti/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable sonarjs/table-header */
 import { NavBar } from '@/components/nav/nav-bar';
 import { address, cellNum, tellNum } from '@/config/config';
 
@@ -11,43 +10,33 @@ const Page = () => {
           <div className="p-[5px] mb-[20px] border-b border-[#dedede]">
             <h1 className="font-normal text-[#333333]">{'Contatti'}</h1>
           </div>
-          <div className="w-full mb-5 leading-[1.9em]">
-            <table border={0} cellPadding={5} cellSpacing={5} className="w-full">
-              <tbody>
-                <tr>
-                  <td>
-                    <p className="text-justify pb-[18px]">
-                      <span className="text-[14px]">
-                        <span className="text-[#008000]">
-                          <strong>
-                            {'Pantelflora'}
-                            <br />
-                            {'di Guida Giuseppe'}
-                          </strong>
-                          <br />
-                        </span>
-                        <br />
-                        {address} {' - 91017 - Pantelleria (TP)'}
-                        <br />
-                        {'Sicilia - Italia'}
-                      </span>
-                    </p>
-                  </td>
-                  <td>
-                    <span className="text-[14px]">
-                      <span>
-                        {'Tel. '} {tellNum} <br />
-                        {'Cell. '} {cellNum} {' (Michael)'}
-                        <br />
-                        <br />
-                        {/* E-mail:  */}
-                        <br />
-                      </span>
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="w-full mb-5 leading-[1.9em] grid md:grid-cols-2">
+            <div className="text-justify pb-[18px]">
+              <span className="text-[14px]">
+                <span className="text-[#008000]">
+                  <strong>
+                    {'Pantelflora'}
+                    <br />
+                    {'di Guida Giuseppe'}
+                  </strong>
+                  <br />
+                </span>
+                <br />
+                {address} {' - 91017 - Pantelleria (TP)'}
+                <br />
+                {'Sicilia - Italia'}
+              </span>
+            </div>
+            <span className="text-[14px]">
+              <span>
+                {'Tel. '} {tellNum} <br />
+                {'Cell. '} {cellNum} {' (Michael)'}
+                <br />
+                <br />
+                {/* E-mail:  */}
+                <br />
+              </span>
+            </span>
           </div>
         </div>
       </div>
